d53bc5505d61d3d681b43d1f9d4ec85164975261
Lale Cumayeva <100639782+LalaJumayeva@users.noreply.github.com>
2023-03-13 22:15:11 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,11 @@
 import React, { useState } from "react";
 
 function App() {
-  const [previous, next] = useState("0");
-
-  function subtract() {
-    next(previous - 1);
-  }
-
-  function add() {
-    next(previous + 1);
-  }
+ 
 
   return (
     <div id="div">
-      <button onClick={subtract}>-</button>
-      <h1>{previous}</h1>
-      <button onClick={add}>+</button>
+
     </div>
   );
 }
