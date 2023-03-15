bf9707c81dad4b8887a1f1b95d4706622ce82f83
Lale Cumayeva <100639782+LalaJumayeva@users.noreply.github.com>
2023-03-15 21:43:42 +0100

    fetching data functions added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,27 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import MovieCard from "./Moviecard";
 import SearchIcon from "../components/search.svg";
+import tmdb from "./tmdb";
 
-const movie1 = {
-  adult: false,
-  backdrop_path: "/22z44LPkMyf5nyyXvv8qQLsbom.jpg",
-  genre_ids: [27, 9648, 53],
-  id: 631842,
-  original_language: "en",
-  original_title: "Knock at the Cabin",
-  overview:
-    "While vacationing at a remote cabin, a young girl and her two fathers are taken hostage by four armed strangers who demand that the family make an unthinkable choice to avert the apocalypse. With limited access to the outside world, the family must decide what they believe before all is lost.",
-  popularity: 2101.02,
-  poster_path: "/dm06L9pxDOL9jNSK4Cb6y139rrG.jpg",
-  release_date: "2023-02-01",
-  title: "Knock at the Cabin",
-  video: false,
-  vote_average: 6.5,
-  vote_count: 983
-};
+// const movie1 = {
+//   adult: false,
+//   backdrop_path: "/22z44LPkMyf5nyyXvv8qQLsbom.jpg",
+//   genre_ids: [27, 9648, 53],
+//   id: 631842,
+//   original_language: "en",
+//   original_title: "Knock at the Cabin",
+//   overview:
+//     "While vacationing at a remote cabin, a young girl and her two fathers are taken hostage by four armed strangers who demand that the family make an unthinkable choice to avert the apocalypse. With limited access to the outside world, the family must decide what they believe before all is lost.",
+//   popularity: 2101.02,
+//   poster_path: "/dm06L9pxDOL9jNSK4Cb6y139rrG.jpg",
+//   release_date: "2023-02-01",
+//   title: "Knock at the Cabin",
+//   video: false,
+//   vote_average: 6.5,
+//   vote_count: 983
+// };
+
+const API_search = "https://api.themoviedb.org/3/movie/search/movie?api_key=7736ed9f5427a468429b0f9cc1d80060&language=en-US&page=1";
 
 const URL =
   "https://api.themoviedb.org/3/movie/popular?api_key=7736ed9f5427a468429b0f9cc1d80060&language=en-US&page=1";
@@ -29,15 +31,19 @@ function App() {
   const [search, setSeatch] = useState("");
 
   useEffect(() => {
-    axios
-      .get(URL)
-      .then((res) => {
-        setMovie(res.data.results);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
+    const fetchMovies = async() =>{
+      const {data} = await tmdb.get("movie/popular")
+      setMovie(data.results)
+    }
+    fetchMovies()
+  }, []);
+
+  const handleSearch= (e) => {
+    e.preventDefault();
+    fetch(API_search + search)
+    .then(res => res.json())
+    .then(data => setMovie(data.results))
+  }
 
   return (
     <div className="app">
@@ -49,7 +55,7 @@ function App() {
           value={search}
           onChange={(e) => setSeatch(e.target.value)}
         />
-        <img src={SearchIcon} alt="search" onClick={() => {}} />
+        <img src={SearchIcon} alt="search" onClick={handleSearch} />
       </div>
       {movies?.length > 0 ? (
         <div className="container">
